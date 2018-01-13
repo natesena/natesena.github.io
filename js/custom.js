74961506f4c8dd6738d24f8c9d7ef95f641df29f
Nathaniel Sena <nate.sena1@gmail.com>
2018-01-13 12:40:00 -0800

    better scrollbar

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,32 @@
-// ------------------------------
-// http://twitter.com/mattsince87
-// ------------------------------
+//on scroll up navbar should appear
+var navEl = document.getElementById('theNavBar')
+var contentEl = document.getElementById('content-container')
 
-function scrollNav() {
-  $('.nav a').click(function(){  
-    //Toggle Class
-    $(".active").removeClass("active");      
-    $(this).closest('li').addClass("active");
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('li').addClass('active');
-    //Animate
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top - 160
-    }, 400);
-    return false;
-  });
-  $('.scrollTop a').scrollTop();
+console.log("nav height", navEl.clientHeight)
+
+window.addEventListener('scroll', scrollTopDisappear)
+var lastScroll = 0
+function scrollTopDisappear(){
+  var scrollMeter = window.pageYOffset
+  if(scrollMeter < lastScroll){//if scrolling up
+    if(scrollMeter > navEl.clientHeight){
+      //console.log('navbar should be colored')
+      $('.navbar-inverse').addClass("navbar-colored")
+    }
+    else{
+      $('.navbar-inverse').removeClass("navbar-colored")
+    }
+    //console.log('show nav')
+    $('.navbar').fadeIn()
+  }
+  else{
+    //console.log('hide nav')
+    $('.navbar').fadeOut()
+  }
+  lastScroll = scrollMeter
 }
-scrollNav();
+
+function scrollToTop(){
+  console.log('hey')
+  $('html,body').animate({ scrollTop: 0 }, 'slow');
+}
